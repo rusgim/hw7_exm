5b92c343d8cbb1a2cee238e457b0a85d0e16cc8c
Ruslan <gimazevs@mail.ru>
2022-10-09 11:03:43 +0300

    сделал дз

--- src/Main.java
+++ src/Main.java
@@ -52,7 +52,7 @@ public class Main {
         int salaryPercentages = 29000;
         int total2 = 0;
         for (int p = 1; p <= 12; p++) {
-            total2 = (int) (total2 + total2 * 0.01);
+            total2 = (total2 + total2 / 100);
             total2 = total2 + salaryPercentages;
             System.out.println("Месяц " + p + " , сумма накоплений равна " + total2 + " рублей");
         }
